62e01aa07fb3cd12405fa244889be59444090610
Auspicious Soft <60027228+auspicious-soft@users.noreply.github.com>
2025-01-14 09:54:03 +0530

    Maity added in title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NeurCG",
+  title: "Maity 🚀",
   description: "",
 };
 
