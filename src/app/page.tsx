8531a90ee022bd4172ac6d965a46df64a456ba2c
vrodriguezpas <156944235+vrodriguezpas@users.noreply.github.com>
2025-02-18 16:52:16 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
         <div>
           <div className="relative pt-[50px]">
             <div className="video-frame">
-              <VideoPlayer url="/assets/homevideo1.mp4" />
+              <VideoPlayer url="/assets/Finalvideo.mp4" />
             </div>
             <div className="hidden md:flex p-[7px] items-center bg-white gap-4 rounded-lg pr-5 absolute top-[25%] translate-y-[-25%] left-[50px]  ">
               <VideoIcon1 />
