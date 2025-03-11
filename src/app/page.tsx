7f10dbd6e932431708b2bd474d396deb661cdeae
vrodriguezpas <156944235+vrodriguezpas@users.noreply.github.com>
2025-03-11 13:02:05 +0100

    Update page.tsx
    Delete attempt 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ const StepCardData = [
     id: 3,
     title: "Interactive Q&A",
     description: "Users can ask questions, and the avatar responds automatically, before continuing with the class or course seamlessly.",
-    <VideoPlayer url="/assets/Avatarhomevideo.mp4" /> //imgSrc: stepimg3,
+    imgSrc: stepimg3,
   },
 ]
 export default function Home() {
