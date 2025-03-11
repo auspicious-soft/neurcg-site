d810543f70a6d61d47675f6bed2076bcfdf2cd5a
vrodriguezpas <156944235+vrodriguezpas@users.noreply.github.com>
2025-03-11 13:58:10 +0100

    Update page.tsx
    Attempt 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ const StepCardData = [
     id: 3,
     title: "Interactive Q&A",
     description: "Users can ask questions, and the avatar responds automatically, before continuing with the class or course seamlessly.",
-    videoSrc: "public/assets/maity.mp4",
+    videoSrc: "/assets/Avatarhomevideo.mp4",
   },
 ]
 export default function Home() {
