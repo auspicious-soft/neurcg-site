d1b9146c7b9595fbab4a47e38912db218c78b92c
vrodriguezpas <156944235+vrodriguezpas@users.noreply.github.com>
2025-03-11 15:13:29 +0100

    Update page.tsx
    correct error in new video name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ const StepCardData = [
     id: 3,
     title: "Interactive Q&A",
     description: "Users can ask questions, and the avatar responds automatically, before continuing with the class or course seamlessly.",
-    videoSrc: "/assets/Maity_Short.mp4
+    videoSrc: "/assets/Maity_Short.mp4"
 .mp4",
   },
 ]
